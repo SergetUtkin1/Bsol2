e5cec01ea34a8a2d6d39dab3d4aaf39d6967223a
Sergey_Utkin <sergey.uty.uty@gmail.com>
2022-03-17 23:00:14 +0300

    Добавьте файлы проекта.

---
+++ Bsol2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x, y;
+	cout << "Введите координаты x и y \n";
+	cin >> x >> y;
+	if (!(x <= 1 && x >= -1 && y <= 1 && y >= -1) && (x < 2 && x > -2 && y < 2 && y > -2))
+	{
+		cout << "Точка принадлежит фигуре";
+	}
+	else
+	{
+		cout << "Точка принадлежит фигуре";
+	}
+}
